676045ecee682bc31e1e1827c8bda55bcdfe44ff
cozyGalvinism <jean@der-capta.in>
2024-07-22 11:42:47 +0200

    Only initialize once

--- byers/src/event_handlers/ready.rs
+++ byers/src/event_handlers/ready.rs
@@ -1,17 +1,24 @@
 use std::sync::Arc;
 
 use clokwerk::AsyncScheduler;
+use clokwerk::TimeUnits;
 use fred::{prelude::PubsubInterface, types::RedisValue};
+use lazy_static::lazy_static;
 use poise::serenity_prelude::*;
 use tracing::{debug, info};
 use tracing_unwrap::ResultExt;
-use clokwerk::TimeUnits;
 
 use crate::prelude::*;
 use judeharley::{
     communication::ByersUnixStream,
-    prelude::{ServerChannelConfig, Songs}, sea_orm::{ActiveValue, DatabaseConnection},
+    prelude::{ServerChannelConfig, Songs},
+    sea_orm::{ActiveValue, DatabaseConnection},
 };
+
+lazy_static! {
+    static ref INITIALIZED: std::sync::atomic::AtomicBool =
+        std::sync::atomic::AtomicBool::new(false);
+}
 
 async fn spawn_subscriber_handler(
     data: &Data<ByersUnixStream>,
@@ -52,16 +59,23 @@ async fn remind_users_to_hydrate(http: Arc<Http>, db: DatabaseConnection) {
     for channel in hydration_channels {
         if let Some(last_message_sent) = channel.last_message_sent.as_ref() {
             // if the last message was sent more than 15 minutes ago, don't remind
-            if last_message_sent < &(chrono::Utc::now().naive_utc() - chrono::Duration::minutes(15)) {
+            if last_message_sent < &(chrono::Utc::now().naive_utc() - chrono::Duration::minutes(15))
+            {
                 continue;
             }
         }
 
-        channel.update(judeharley::entities::server_channel_config::ActiveModel {
-            id: ActiveValue::set(channel.id),
-            last_message_sent: ActiveValue::set(Some(chrono::Utc::now().naive_utc())),
-            ..Default::default()
-        }, &db).await.expect_or_log("Failed to update hydration channel");
+        channel
+            .update(
+                judeharley::entities::server_channel_config::ActiveModel {
+                    id: ActiveValue::set(channel.id),
+                    last_message_sent: ActiveValue::set(Some(chrono::Utc::now().naive_utc())),
+                    ..Default::default()
+                },
+                &db,
+            )
+            .await
+            .expect_or_log("Failed to update hydration channel");
 
         let discord_channel_id = ChannelId::new(channel.id as u64);
 
@@ -86,21 +100,26 @@ pub async fn on_ready(
 ) -> Result<(), crate::prelude::Error> {
     info!("Connected as {}", data_about_bot.user.name);
 
-    spawn_subscriber_handler(data, ctx).await?;
+    if !INITIALIZED.load(std::sync::atomic::Ordering::Relaxed) {
+        spawn_subscriber_handler(data, ctx).await?;
 
-    let mut scheduler = AsyncScheduler::new();
+        let mut scheduler = AsyncScheduler::new();
 
-    let http_clone = ctx.http.clone();
-    let db_clone = data.db.clone();
-    scheduler.every(15.minutes())
-        .run(move || remind_users_to_hydrate(http_clone.to_owned(), db_clone.to_owned()));
+        let http_clone = ctx.http.clone();
+        let db_clone = data.db.clone();
+        scheduler
+            .every(15.minutes())
+            .run(move || remind_users_to_hydrate(http_clone.to_owned(), db_clone.to_owned()));
 
-    tokio::spawn(async move {
-        loop {
-            scheduler.run_pending().await;
-            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-        }
-    });
+        tokio::spawn(async move {
+            loop {
+                scheduler.run_pending().await;
+                tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+            }
+        });
+
+        INITIALIZED.store(true, std::sync::atomic::Ordering::Relaxed);
+    }
 
     let current_song = Songs::last_played(&data.db).await;
     if let Ok(Some(current_song)) = current_song {
